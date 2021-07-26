ef8e021d284455193579dbdf6cc2aaae1720e0c2
윤예준 <dbm01049@gmail.com>
2021-07-26 10:03:28 +0900

    2021-07-26

---
+++ BOJ_2021/july/BOJ2869.java
@@ -0,0 +1,27 @@
+package july;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ2869 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		int result = (int) Math.ceil((V - B) / (A - B));
+
+
+		bw.append((result) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+}
